c628a3e5ece9793dc1afa29aa9bbd6d63cbbbcdd
mrunknown850 <minh0608@outlook.com>
2025-03-26 20:25:13 +0700

    Implement points as replacement for vectors to represent positions in space.

--- backend/src/physics/Math.cpp
+++ backend/src/physics/Math.cpp
@@ -1,5 +1,6 @@
 #include "Math.hpp"
 
+// Defining vectors
 Vec3::Vec3() : x(0), y(0), z(0) {}
 Vec3::Vec3(double x, double y, double z) : x(x), y(y), z(z) {}
 Vec3 Vec3::operator+(const Vec3 &other) const { return Vec3(x + other.x, y + other.y, z + other.z); }
@@ -19,4 +20,19 @@ double Vec3::operator*(const Vec3 &other) const
 double Vec3::LengthSquared() const
 {
     return x * x + y * y + z * z;
+}
+
+// Defining points
+Point3::Point3() : x(0), y(0), z(0) {}
+Point3::Point3(double x, double y, double z) : x(x), y(y), z(z) {}
+Point3 Point3::operator+(const Vec3 &other) const { return Point3(x + other.x, y + other.y, z + other.z); }
+Vec3 Point3::operator-(const Point3 &other) const { return Vec3(x - other.x, y - other.y, z - other.z); }
+
+// Utils
+double PointDistanceSquared(const Point3 &p1, const Point3 &p2)
+{
+    double sx = p1.x - p2.x;
+    double sy = p1.y - p2.y;
+    double sz = p1.z - p2.z;
+    return sx * sx + sy * sy + sz * sz;
 }
--- backend/tests/test_math.cpp
+++ backend/tests/test_math.cpp
@@ -3,6 +3,7 @@
 
 constexpr double epsilon = 1e-6;  // Allowable error margin
 
+// Testing vectors
 TEST(VectorOperations, VectorAdditionTest)
 {
     Vec3 v1(1.5, 2.6, 3.87);
@@ -36,4 +37,30 @@ TEST(VectorOperations, VectorLengthSquaredTest)
     Vec3 v1(1.5, 2.6, 3.87);
     EXPECT_NEAR(v1.LengthSquared(), 1.5 * 1.5 + 2.6 * 2.6 + 3.87 * 3.87, epsilon);
     EXPECT_NEAR(v1.LengthSquared(), v1 * v1, epsilon);
+}
+
+// Testing points
+TEST(PointOperations, PointDisplacementTest) {
+    Point3 p(1, 5, 2);
+    Vec3 v(0, 5, 2);
+    Point3 ps = p + v;
+    EXPECT_NEAR(ps.x, p.x + v.x, epsilon);
+    EXPECT_NEAR(ps.y, p.y + v.y, epsilon);
+    EXPECT_NEAR(ps.z, p.z + v.z, epsilon);
+}
+
+TEST(PointOperations, VectorDifferenceTest) {
+    Point3 p1(1, 5, 2);
+    Point3 p2(0, 5, 2);
+    Vec3 vs = p1 - p2;
+    EXPECT_NEAR(vs.x, p1.x - p2.x, epsilon);
+    EXPECT_NEAR(vs.y, p1.y - p2.y, epsilon);
+    EXPECT_NEAR(vs.z, p1.z - p2.z, epsilon);
+}
+
+TEST(PointOperations, PointDistanceTest) {
+    Point3 p1(1, 5, 2);
+    Point3 p2(0, 5, 2);
+    double d = PointDistanceSquared(p1, p2);
+    EXPECT_NEAR(d, 1, epsilon);
 }
